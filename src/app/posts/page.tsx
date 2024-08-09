35a5767ba3d7524ec1e82d6414d97b881fbb4532
Kelvin César de Andrade <kelvincandrade@gmail.com>
2024-08-08 23:26:35 -0300

    feature: data collection server component

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,7 +1,23 @@
-export default function Page() {
+import Link from "next/link";
+
+export default async function Page() {
+    const response = await fetch("https://dummyjson.com/posts?limit=10");
+    const data = await response.json();
+
     return (
-        <main className="text-center pt-32 px-5">
+        <main className="text-center pt-16 px-5">
           <h1 className="text-4xl md:text-5xl font-bold mb-5"> All posts </h1>
+
+          <ul> 
+            {
+                data.posts.map(post => (
+                    <li key={post.id} className="mb-3">
+                        <Link href={`/posts/${post.id}`} className="text-2xl font-semibold">{post.title}</Link>
+                    </li>
+                ))
+            }
+
+          </ul>
         </main>
       );
 }
