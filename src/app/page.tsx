c12e6c730982c52d62e518c9ecaa35bc37b90472
Kelvin César de Andrade <kelvincandrade@gmail.com>
2024-08-08 22:37:33 -0300

    improvement: added posts route, and some tailwind tags - md:text-5xl to define font size depending on the screen size; - background and text color based on docs (https://tailwindcss.com/docs/customizing-colors)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,10 @@
 export default function Home() {
   return (
     <main>
-      Home
+      <h1 className="text-4xl md:text-5xl font-bold mb-5"> Welcome to my blog! </h1>
+      <p>
+        I'm a electrical engineer with love on programming. I'm currently learning this technology called Next JS!
+      </p>
     </main>
   );
 }
