832757822893598186a50a59ff44f4b4fb2fea94
Kelvin César de Andrade <kelvincandrade@gmail.com>
2024-08-08 22:52:50 -0300

    feature: added navigation on header

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,16 +1,37 @@
 import Image from "next/image";
+import Link from "next/link";
+
+const navLinks = [
+    {
+        href: "/",
+        label: "Home",
+    },
+    {
+        href: "/posts",
+        label: "Posts",
+    }
+];
+
 export default function Header() {
     return (
         <header>
-          <Image 
-            src="https://avatars.githubusercontent.com/u/55962427?v=4&size=35"
-            alt="Blog logo"
-            className="rounded-full w-[35px] h-[35px]"
-            width="35"
-            height="35"
-          />
-        </header>
-
+            <Image 
+                src="https://avatars.githubusercontent.com/u/55962427?v=4&size=35"
+                alt="Blog logo"
+                className="rounded-full w-[35px] h-[35px]"
+                width="35"
+                height="35"
+            />
         
+            <nav>
+                <ul>
+                {navLinks.map((link) => (
+                    <li key={link.href}>
+                        <Link href={link.href}>{link.label}</Link>
+                    </li>
+                ))}
+                </ul>
+            </nav>
+        </header>
       );
 }
