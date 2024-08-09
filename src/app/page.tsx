c0a2741494c11d08aa001b5d88842bf6c2c630d9
Kelvin César de Andrade <kelvincandrade@gmail.com>
2024-08-08 22:39:49 -0300

    improvement: created center tags

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@
 
 export default function Home() {
   return (
-    <main>
+    <main className="text-center pt-32 px-5">
       <h1 className="text-4xl md:text-5xl font-bold mb-5"> Welcome to my blog! </h1>
-      <p>
+      <p className="max-w[750px] mx-auto leading-8">
         I'm a electrical engineer with love on programming. I'm currently learning this technology called Next JS!
       </p>
     </main>
